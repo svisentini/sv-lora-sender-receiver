6c4da7cbd52779280b5243d5b532f0f1f232dafe
Sidao <svisentini@gmail.com>
2024-06-10 23:50:16 -0300

    V2 without display messagens

---
+++ telemetria_dae_V2_SemMensagem/telemetria_receiver/telemetria_receiver.ino
@@ -0,0 +1,63 @@
+#include "heltec.h"
+ 
+#define BAND 915E6
+#define pinRele 32
+
+char st;
+unsigned long contador;
+
+void setup () {
+  Heltec.begin(false, true, true, true, BAND);
+
+  //Heltec.display->clear();
+  delay(100);
+  LoRa.setSpreadingFactor(12);
+  LoRa.setSignalBandwidth(250E3);
+  LoRa.setCodingRate4(5);
+  LoRa.setPreambleLength(6);
+  LoRa.setSyncWord(0x12);
+  LoRa.crc();
+  delay(100);
+
+
+  pinMode(pinRele, OUTPUT);
+  contador = 0UL;
+  
+  //Heltec.display->init();
+  //Heltec.display->flipScreenVertically();
+  //Heltec.display->setFont(ArialMT_Plain_16);
+  //  Heltec.display->drawString(0, 0,  "RECEIVER");
+  //  Heltec.display->drawString(0, 20, "Telemetria");
+  //  Heltec.display->drawString(0, 40, "Esperando ..." );
+  //Heltec.display->display();
+  delay(1000);
+
+  LoRa.onReceive(recebido);
+  LoRa.receive();
+}
+ 
+void loop () {
+
+  if (st == '1') {
+    digitalWrite(pinRele, 1);
+    delay(200);
+    digitalWrite(pinRele, 0);
+    
+    //Heltec.display->init();
+    //Heltec.display->flipScreenVertically();
+  //Heltec.display->setFont(ArialMT_Plain_16);
+    //Heltec.display->drawString(0, 0,  "RECEIVER");
+    //Heltec.display->drawString(0, 20, "RSSI: " + String(LoRa.packetRssi(), DEC) );
+    //Heltec.display->drawString(0, 40, "Recebidos: " + String(contador) );
+    //Heltec.display->display();
+    delay(500);
+    
+    st = '0';
+  }
+
+}
+
+void recebido(int packetSize) {
+  st = '1';
+  contador++;
+}
---
+++ telemetria_dae_V2_SemMensagem/telemetria_sender/telemetria_sender.ino
@@ -0,0 +1,61 @@
+#include "heltec.h"
+#define BAND 915E6
+
+int SENSOR = 32;
+const int ACTIVE = 1;
+int lastState = 1;
+int currentState = 1;
+unsigned long contador;
+
+void setup () {
+  Heltec.begin(true, true, true, true, BAND);
+
+  Heltec.display->clear();
+  delay(100);
+  LoRa.setSpreadingFactor(12);
+  LoRa.setSignalBandwidth(250E3);
+  LoRa.setCodingRate4(5);
+  LoRa.setPreambleLength(6);
+  LoRa.setSyncWord(0x12);
+  LoRa.crc();
+  delay(100);
+
+  pinMode(SENSOR, INPUT);
+  contador = 0UL;
+
+  Heltec.display->init();
+  Heltec.display->flipScreenVertically();
+  Heltec.display->setFont(ArialMT_Plain_16);
+    Heltec.display->drawString(0, 0,  "SENDER");
+    Heltec.display->drawString(0, 20, "Telemetria");
+    Heltec.display->drawString(0, 40, "Esperando ..." );
+  Heltec.display->display();
+  delay(1000);
+
+}
+
+void loop () {
+  currentState = digitalRead(SENSOR);
+
+  if (lastState == 0 && currentState == 1){
+    contador++;
+    LoRa.beginPacket();
+    // LoRa.setTxPower(20,RF_PACONFIG_PASELECT_PABOOST);
+    LoRa.print(contador);
+    LoRa.endPacket();
+
+    Heltec.display->init();
+    Heltec.display->flipScreenVertically();
+    Heltec.display->setFont(ArialMT_Plain_16);
+    //Heltec.display->drawString(0, 0,  "SENDER");
+    //Heltec.display->drawString(0, 20, "Telemetria");
+    //Heltec.display->drawString(0, 40, "Contador: " + String(contador) );
+    Heltec.display->drawString(0, 40, String(contador) );
+    Heltec.display->display();
+    delay(100);
+
+  }
+  lastState = currentState;
+
+
+}
